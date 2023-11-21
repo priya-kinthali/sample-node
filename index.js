c634a5e422bf6c510ce8b5406e2944cd7fdb5b19
Priya Gupta <priya.gupta10@ad.infosys.com>
2023-11-21 19:14:58 +0530

    Added files/folders

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
