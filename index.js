9f70b1642138b094cfd9c409dd7c4442f956098e
priya-kinthali <priya-kinthali@github.com>
2025-03-12 11:25:25 +0530

    with import

--- index.js
+++ index.js
@@ -13,4 +13,4 @@
 import messenger from 'webext-messenger';
 const unusedVar = 42; // This should trigger a warning/error
 console.log('Hello, world!'); // This should be fine
-console.log(messenger); // messenger
+// console.log(messenger); // messenger
